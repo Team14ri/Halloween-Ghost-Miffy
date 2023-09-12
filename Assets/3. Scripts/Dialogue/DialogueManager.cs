038a98c604e485c9d2b884054f435d8d983888fb
LiF <leesj.dev@gmail.com>
2023-09-12 13:49:47 +0900

    fix: 폰트 설정 변경

--- Assets/3. Scripts/Dialogue/DialogueManager.cs
+++ Assets/3. Scripts/Dialogue/DialogueManager.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DS
 {
     public class DialogueManager : MonoBehaviour
     {
+        private Button _playDialogueButton;
+        
         private void Start()
+        {
+            _playDialogueButton.onClick.AddListener(PlayDialogue);
+        }
+
+        private void PlayDialogue()
         {
             string input = "테스트 테스트 <speed:5>속도 증가 <pause:1><anim:wave>애니메이션</anim><speed:1>속도 보통 <pause:3>3초 멈춤";
             List<DialogueUtility.Command> commands = DialogueUtility.ParseCommands(input);
@@ -15,11 +24,6 @@ namespace DS
             {
                 Debug.Log($"{command.commandType}, {command.textAnimationType}, \"{command.stringValue}\", {command.floatValue}");
             }
-        }
-
-        private void PlayDialogue()
-        {
-            
         }
     }
 }
