216faf49b6dc31eae56c6617898e2203b848bdfe
LiF <leesj.dev@gmail.com>
2023-09-23 22:17:49 +0900

    chore: 폴더 위치 변경

--- Assets/3. Scripts/Dialogue/Editor/DialogueGraph.cs
+++ Assets/3. Scripts/Dialogue/Editor/DialogueGraph.cs
@@ -7,6 +7,7 @@ using UnityEngine.UIElements;
 public class DialogueGraph : EditorWindow
 {
     private DialogueGraphView _graphView;
+    private string _fileName = "New Narrative";
     
     [MenuItem("Graph/Dialogue Graph")]
     public static void OpenDialogueGraphView()
@@ -41,13 +42,36 @@ public class DialogueGraph : EditorWindow
     {
         var toolbar = new Toolbar();
 
-        var nodeCreateButton = new Button(() => { _graphView.CreateNode("Dialogue Node"); })
+        var fileNameTextField = new TextField("File Name:");
+        fileNameTextField.SetValueWithoutNotify(_fileName);
+        fileNameTextField.MarkDirtyRepaint();
+        fileNameTextField.RegisterValueChangedCallback(evt => _fileName = evt.newValue);
+        toolbar.Add(fileNameTextField);
+        
+        toolbar.Add(new Button(SaveData)
+        {
+            text = "Save Data"
+        });
+        toolbar.Add(new Button(LoadData)
+        {
+            text = "Load Data"
+        });
+        
+        toolbar.Add(new Button(() => _graphView.CreateNode("Dialogue Node"))
         {
             text = "Create Node"
-        };
-
-        toolbar.Add(nodeCreateButton);
+        });
         
         rootVisualElement.Add(toolbar);
+    }
+    
+    private void SaveData()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void LoadData()
+    {
+        throw new NotImplementedException();
     }
 }
