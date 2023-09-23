7b43fb2b13a2890758caea93b5e352d58530afeb
LiF <leesj.dev@gmail.com>
2023-09-23 23:54:22 +0900

    feat: 저장 기능 완성

--- Assets/3. Scripts/Dialogue/Editor/GraphSaveUtility.cs
+++ Assets/3. Scripts/Dialogue/Editor/GraphSaveUtility.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GraphSaveUtility
 {
     private DialogueGraphView _targetGraphView;
-    
+
+    private List<Edge> Edges => _targetGraphView.edges.ToList();
+    private List<DialogueNode> Nodes => _targetGraphView.nodes.ToList().Cast<DialogueNode>().ToList();
+
     public static GraphSaveUtility GetInstance(DialogueGraphView targetGraphView)
     {
         return new GraphSaveUtility
@@ -14,7 +21,41 @@ public class GraphSaveUtility
 
     public void SaveGraph(string fileName)
     {
+        if (!Edges.Any())   // Edges(연결)이 없다면 저장하지 않음
+            return;
+
+        var dialogueContainer = ScriptableObject.CreateInstance<DialogueContainer>();
+
+        var connectedPorts = Edges.Where(x => x.input.node != null).ToArray();
+        for (int i = 0; i < connectedPorts.Length; i++)
+        {
+            var outputNode = connectedPorts[i].output.node as DialogueNode;
+            var inputNode = connectedPorts[i].input.node as DialogueNode;
+            
+            dialogueContainer.NodeLinks.Add(new NodeLinkData
+            {
+                BaseNodeGuid = outputNode.GUID,
+                PortName = connectedPorts[i].output.portName,
+                TargetNodeGuid = inputNode.GUID
+            });
+        }
+
+        foreach (var dialogueNode in Nodes.Where(node => !node.EntryPoint))
+        {
+            dialogueContainer.DialogueNodeData.Add(new DialogueNodeData
+            {
+                GUID = dialogueNode.GUID,
+                DialogueText = dialogueNode.DialogueText,
+                Position = dialogueNode.GetPosition().position
+            });
+        }
+
+        // 리소스 폴더가 없다면 자동으로 폴더 생성
+        if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+            AssetDatabase.CreateFolder("Assets", "Resources");
         
+        AssetDatabase.CreateAsset(dialogueContainer, $"Assets/Resources/{fileName}.asset");
+        AssetDatabase.SaveAssets();
     }
 
     public void LoadGraph(string fileName)
