224b25a37b4253e708fe6ae159f84f72b044a076
LiF <leesj.dev@gmail.com>
2023-10-07 14:55:50 +0900

    refactor: 포탈 관련 코드 일부 수정

--- Assets/3. Scripts/Portal/Portal.cs
+++ Assets/3. Scripts/Portal/Portal.cs
@@ -7,18 +7,18 @@ public class Portal : MonoBehaviour
     [Header("ex) MiffyHouse씬의 1번 포탈로 이동시키고 싶다면?")]
     [Header("-> exitSceneName = MiffyHouse / exitPortalNum = 1")]
     [Space]
-    public string exitSceneName = null;
+    public string exitSceneName;
     public int exitPortalNum = -1;
 
     private void Start()
     {
-        PortalManager.instance.portalDictionary[portalNum] = this;
+        PortalManager.Instance.PortalDictionary[portalNum] = this;
     }
 
     private void Update()
     {
         // 포탈마다 Update문을 돌리지 않고, 플레이어가 포탈을 탐지하게 변경하면 좋을것 같습니다.
-        if (Input.GetKeyDown(KeyCode.Space) && IsPlayerCloseToPortal())
+        if (Input.GetKeyDown(KeyCode.E) && IsPlayerCloseToPortal())
         {
             SceneManagerCustom.instance.LoadScene(exitSceneName, exitPortalNum);
         }
@@ -26,13 +26,13 @@ public class Portal : MonoBehaviour
 
     public bool IsPlayerCloseToPortal()
     {
-        Vector3 portalPos = this.transform.position;
-        Vector3 playerPos = PortalManager.instance.player.transform.position;
+        Vector3 portalPos = transform.position;
+        Vector3 playerPos = PortalManager.Instance.Player.transform.position;
         Vector3 difference = portalPos - playerPos;
 
         float distance = difference.magnitude;
 
         // 플레이어와의 거리가 설정된 거리보다 작거나 같다면 true
-        return distance <= PortalManager.instance.portalActivationRange;
+        return distance <= PortalManager.Instance.portalActivationRange;
     }
 }
