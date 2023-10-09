2a1503b4ae1a45da7770abf118c76e61e6fe91a6
LiF <leesj.dev@gmail.com>
2023-10-09 18:05:06 +0900

    feat: 씬 이동 트랜지션 애니메이션 추가

--- Assets/3. Scripts/Portal/SceneManagerCustom.cs
+++ Assets/3. Scripts/Portal/SceneManagerCustom.cs
@@ -4,22 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerCustom : MonoBehaviour
 {
-    public static SceneManagerCustom instance { get; private set; }
-    private Cutout cutout;
+    public static SceneManagerCustom Instance { get; private set; }
     
     private void Awake()
     {
-        if (instance != null)
+        if (Instance != null)
         {
             Debug.LogWarning("한 씬에 SceneManagerCustom가 여러 개 있어 삭제합니다.");
             Destroy(this.gameObject);
         }
         else
         {
-            instance = this;
+            Instance = this;
             DontDestroyOnLoad(this.gameObject);
-
-            FindCutout();
         }
     }
 
@@ -33,8 +30,6 @@ public class SceneManagerCustom : MonoBehaviour
     
     private IEnumerator LoadSceneCoroutine(string sceneName, int exitPortalNum)
     {
-        yield return StartCoroutine(cutout.FadeOutCoroutine());
-        
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
 
         // 씬 로드가 완료될 때까지 대기
@@ -49,25 +44,10 @@ public class SceneManagerCustom : MonoBehaviour
         {
             Debug.Log("씬 로드 완료");
             PortalManager.Instance.TeleportPlayerToExitPortal(sceneName, exitPortalNum);
-            
-            FindCutout();
-            yield return StartCoroutine(cutout.FadeInCoroutine());
         }
         else
         {
             Debug.LogError("씬 로드 중 예외 발생 또는 씬 활성화가 중지되었습니다.");
-        }
-
-
-    }
-
-    private void FindCutout()
-    {
-        cutout = GameObject.FindObjectOfType<Cutout>();
-
-        if (cutout == null)
-        {
-            Debug.Log("Cutout 스크립트를 가진 객체를 찾지 못했습니다.");
         }
     }
 }
---
+++ Assets/3. Scripts/Tools/SceneTransitionController.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class SceneTransitionController : MonoBehaviour
+{
+    [SerializeField] private RectTransform transitionUI;
+
+    [SerializeField] private float irisMaxValue = 2200f;
+    
+    [SerializeField] private float irisOpenTime = 1f;
+    [SerializeField] private AnimationCurve IrisOpenAccelerationCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
+    
+    [SerializeField] private float irisCloseTime = 1f;
+    [SerializeField] private AnimationCurve IrisCloseAccelerationCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
+    
+    public static SceneTransitionController Instance;
+    
+    private Coroutine _transitionRoutine;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            if (transitionUI == null)
+                return;
+            
+            transitionUI.sizeDelta = new Vector2(0f, 0f);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void IrisOpen(Action action)
+    {
+        if (transitionUI == null)
+            return;
+        
+        this.EnsureCoroutineStopped(ref _transitionRoutine);
+        _transitionRoutine = StartCoroutine(IrisOpenProcess(action));
+    }
+    
+    public void IrisClose(Action action)
+    {
+        if (transitionUI == null)
+            return;
+        
+        this.EnsureCoroutineStopped(ref _transitionRoutine);
+        _transitionRoutine = StartCoroutine(IrisCloseProcess(action));
+    }
+    
+    private IEnumerator IrisOpenProcess(Action action)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < irisOpenTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float percentage = elapsedTime / irisOpenTime;
+
+            float curveValue = IrisOpenAccelerationCurve.Evaluate(percentage);
+            float irisValue = Mathf.Lerp(0, irisMaxValue, curveValue);
+            transitionUI.sizeDelta = new Vector2(irisValue, irisValue);
+            yield return null;
+        }
+        transitionUI.sizeDelta = new Vector2(irisMaxValue, irisMaxValue);
+        action?.Invoke();
+    }
+    
+    private IEnumerator IrisCloseProcess(Action action)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < irisCloseTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float percentage = elapsedTime / irisCloseTime;
+
+            float curveValue = IrisCloseAccelerationCurve.Evaluate(percentage);
+            float irisValue = Mathf.Lerp(irisMaxValue, 0, curveValue);
+            transitionUI.sizeDelta = new Vector2(irisValue, irisValue);
+            yield return null;
+        }
+        transitionUI.sizeDelta = new Vector2(irisMaxValue, irisMaxValue);
+        action?.Invoke();
+    }
+}
