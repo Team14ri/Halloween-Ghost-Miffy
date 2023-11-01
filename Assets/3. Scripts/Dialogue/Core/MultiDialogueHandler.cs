870c7bf4476b522ad19b3d5450907a5ff7ba2b38
LiF <leesj.dev@gmail.com>
2023-11-01 17:39:33 +0900

    fix: 선택지가 두 번 연속 나오는 경우 발생하는 오류를 수정

--- Assets/3. Scripts/Dialogue/Core/MultiDialogueHandler.cs
+++ Assets/3. Scripts/Dialogue/Core/MultiDialogueHandler.cs
@@ -85,7 +85,6 @@ namespace DS.Core
         {
             dialogueSetter.nameBox.text = "";
             dialogueSetter.textBox.text = "";
-            choiceDataList.Clear();
         }
         
         private void InitIndex()
