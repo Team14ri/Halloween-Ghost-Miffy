da512bc0cf720a8b77119b1c45532058e1456f23
LiF <leesj.dev@gmail.com>
2023-11-01 10:11:20 +0900

    feat: Multi Choice 말풍선 작업 중

---
+++ Assets/3. Scripts/Dialogue/Core/MultiDialogueHandler.cs
@@ -0,0 +1,19 @@
+using System;
+using Cinemachine;
+using TMPro;
+using UnityEngine;
+
+namespace DS.Core
+{
+    public class MultiDialogueHandler : MonoBehaviour
+    {
+        [SerializeField] private DialogueHandler dialogueHandler;
+        [SerializeField] private DialogueSetter dialogueSetter;
+
+        public void PlayDialogue(string text, bool skipTyping = false)
+        {
+            dialogueSetter.nameBox.text = dialogueHandler.name;
+            DialogueManager.Instance.PlayDialogue(dialogueSetter.textBox, text, skipTyping);
+        }
+    }
+}
