f7e53516768fcca0b84d4e09ae6be97077b82f75
LiF <leesj.dev@gmail.com>
2023-11-09 03:25:43 +0900

    feat: Quest Flow를 여러개로 나눠 관리하는 기법으로 변경 및 퀘스트 2-1 추가

--- Assets/3. Scripts/Quest/QuestFlowManager.cs
+++ Assets/3. Scripts/Quest/QuestFlowManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using DS.Core;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
@@ -65,7 +64,9 @@ namespace Quest
         private QuestLocation currentQuestLocation;
         private int currentQuestID; 
         private int currentQuestDetailID; 
-        private int currentQuestFlowID; 
+        private int currentQuestFlowID;
+
+        private bool observeUpdate = true;
         
         private void Awake()
         {
@@ -75,16 +76,9 @@ namespace Quest
                 PlayerPrefs.DeleteAll();
             }
             
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            else if (Instance != this)
-            {
-                Destroy(gameObject);
-            }
+            Instance = this;
         }
-        
+
         private void Start()
         {
             var questInfo = QuestManager.Instance.CurrentQuestInfo;
@@ -98,6 +92,9 @@ namespace Quest
 
         private void Update()
         {
+            if (!observeUpdate)
+                return;
+            
             var questInfo = QuestManager.Instance.CurrentQuestInfo;
             if (currentQuestLocation != (QuestLocation)questInfo[0] || 
                 currentQuestID != questInfo[1] || 
@@ -111,6 +108,12 @@ namespace Quest
                 
                 UpdateScene();
             }
+        }
+        
+        public void ChangeFlowManager(QuestFlowManager newManager)
+        {
+            observeUpdate = false;
+            newManager.gameObject.SetActive(true);
         }
         
         private IEnumerator UpdateFlowEvent(QuestFlow flow)
