155813598bcdf3da05201ddc10d56367ec64e543
4cid <4cid0x0@gmail.com>
2023-11-10 17:23:19 +0900

    test: BGM 테스트 씬 제작

--- Assets/2. Scenes/Develop/4cid0x0/Sound/BGMPlayer.cs
+++ Assets/2. Scenes/Develop/4cid0x0/Sound/BGMPlayer.cs
@@ -7,6 +7,20 @@ public class BGMPlayer : MonoBehaviour
 {
     private EventInstance bgmInstance;
     
+    public static BGMPlayer Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("한 씬에 BGMPlayer가 여러 개 있습니다.");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    
     void Start()
     {
         bgmInstance = SoundManager.Instance.CreateInstance(FMODEvents.Instance.testBGM1);
