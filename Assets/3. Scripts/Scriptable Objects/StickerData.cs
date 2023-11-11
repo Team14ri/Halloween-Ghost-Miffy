7089e7a359b08c65389735762f97a61e3f4f4e83
LiF <leesj.dev@gmail.com>
2023-11-11 13:53:57 +0900

    feat: 스티커 리소스 추가

---
+++ Assets/3. Scripts/Scriptable Objects/StickerData.cs
@@ -0,0 +1,37 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Sticker@Name", menuName = "Scriptable Objects/Sticker Data")]
+public class StickerData : ScriptableObject
+{
+    [BoxGroup("Default Settings"), Title("Variable ID")]
+    [SerializeField] private string id = "Sticker@Name";
+    public string ID => id;
+
+    [BoxGroup("Sticker Settings"), Title("Sticker Name")]
+    [SerializeField] private new string name;
+    public string Name => name;
+    
+    [BoxGroup("Sticker Settings"), Title("Sticker Description")]
+    [SerializeField, TextArea(3, 10)] private string description;
+    public string Description => description;
+    
+    [BoxGroup("Sticker Settings"), Title("Sticker Image")]
+    [SerializeField] private Sprite sprite;
+    public Sprite Sprite => sprite;
+    
+    [BoxGroup("Sticker Settings"), Title("Sticker Default Value")]
+    [SerializeField] private int defaultValue;
+    public int DefaultValue => defaultValue;
+
+    public void SetValue(int value)
+    {
+        PlayerPrefs.SetInt(id, value);
+    }
+    
+    public int GetValue()
+    {
+        return PlayerPrefs.GetInt(id, defaultValue);
+    }
+}
