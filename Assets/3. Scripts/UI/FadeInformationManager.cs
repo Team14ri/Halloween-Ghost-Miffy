543be518cc60f0c30fce51b22f7d9ccf83055b1d
LiF <leesj.dev@gmail.com>
2023-11-15 22:37:08 +0900

    feat: 컷씬 연출 추가

--- Assets/3. Scripts/UI/FadeInformationManager.cs
+++ Assets/3. Scripts/UI/FadeInformationManager.cs
@@ -1,10 +1,15 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class FadeInformationManager : MonoBehaviour
 {
     [SerializeField] private GameObject informationUI;
     
+    [BoxGroup("Cutscene"), SerializeField] private Image cutsceneField;
+    [BoxGroup("Cutscene"), SerializeField] private Sprite defaultImage;
+
     private TmpTextEditor _fadeInformationTmpTextEditor;
     private UIFadeController _fadeInformationController;
 
@@ -28,6 +33,8 @@ public class FadeInformationManager : MonoBehaviour
     {
         _fadeInformationTmpTextEditor = informationUI.GetComponent<TmpTextEditor>();
         _fadeInformationController = informationUI.GetComponent<UIFadeController>();
+
+        cutsceneField.sprite = defaultImage;
     }
     
     private void ExecuteEvent()
@@ -36,13 +43,15 @@ public class FadeInformationManager : MonoBehaviour
         _unityEvent?.Invoke();
     }
         
-    public void Show(string text, float delay, UnityEvent unityEvent)
+    public void Show(string text, float delay, Sprite cutsceneImage, UnityEvent unityEvent)
     {
         PlayerController.Instance.PlayerInput.enabled = false;
         
         _unityEvent = unityEvent;
-        
+
         Invoke(nameof(ExecuteEvent), _fadeInformationController.GetFadeInTime() + delay);
+        
+        cutsceneField.sprite = cutsceneImage ? cutsceneImage : defaultImage;
         
         _fadeInformationController.SetAutoFadeWaitTime(delay);
         _fadeInformationTmpTextEditor.Edit("Text", text);
