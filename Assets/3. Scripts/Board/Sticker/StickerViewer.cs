fda541ae6bf9829d371197c91096ace44f67210e
LiF <leesj.dev@gmail.com>
2023-11-19 13:14:42 +0900

    feat: 캔디 스티커 리소스 적용

--- Assets/3. Scripts/Board/Sticker/StickerViewer.cs
+++ Assets/3. Scripts/Board/Sticker/StickerViewer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class StickerViewer : MonoBehaviour
 {
+    [SerializeField] private StickerLocation location;
     [SerializeField] private List<StickerItemButton> stickerStorage;
     
     private void Init()
@@ -24,6 +25,7 @@ public class StickerViewer : MonoBehaviour
     {
         var stickerData = VariableManager.Instance.GetStickersList()
             .Where(data => data.GetValue() > 0 && 
+                           data.Location == location &&
                            data.Type == StickerType.Normal)
             .ToList();
 
