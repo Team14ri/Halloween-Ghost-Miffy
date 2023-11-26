c0198ae0a9e51342eae3dfed4f1601775303cf70
LiF <leesj.dev@gmail.com>
2023-11-26 18:06:27 +0900

    feat: UI 사운드 추가 및 EnterUI 버그 수정

--- Assets/3. Scripts/UI/UIManager.cs
+++ Assets/3. Scripts/UI/UIManager.cs
@@ -47,6 +47,7 @@ public class UIManager : MonoBehaviour
             EscapeOneUI();
             return;
         }
+        _uiStack.Last().SetActive(false);
         _uiStack.Push(obj);
         obj.SetActive(true);
         SoundManager.Instance.PlaySound("UI_QuestOpen");
@@ -100,6 +101,7 @@ public class UIManager : MonoBehaviour
                 return;
             }
             EscapeOneUI();
+            SoundManager.Instance.PlaySound("UI_Button_Click");
         }
 
         if (context.canceled) { }
